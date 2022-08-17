8c4efc015554aa805151de486cca48c8488945b5
Landon Cayia <landon.cayia@gmail.com>
2022-08-17 12:00:09 -0400

    Make intensity slider work

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -42,6 +42,7 @@ struct ContentView: View {
                     Text("Intensity")
                     
                     Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity) { _ in applyProcessing() }
                 }
                 .padding(.vertical)
                 
